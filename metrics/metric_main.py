041f812a144053a966a3f1d20091a736e747d100
franocella <f.nocella1@studenti.unipi.it>
2025-06-26 14:57:00 +0200

    Added metrics for smaller datasets, fid1k, fid5k, kid1k and kid5k

--- metrics/metric_main.py
+++ metrics/metric_main.py
@@ -83,10 +83,41 @@ def report_metric(result_dict, run_dir=None, snapshot_pkl=None):
 # Recommended metrics.
 
 @register_metric
+def fid1k(opts):
+    opts.dataset_kwargs.update(max_size=None, xflip=False)
+    fid = frechet_inception_distance.compute_fid(opts, max_real=None, num_gen=1000)
+    return dict(fid1k=fid)
+
+@register_metric
+def fid5k(opts):
+    opts.dataset_kwargs.update(max_size=None, xflip=False)
+    fid = frechet_inception_distance.compute_fid(opts, max_real=None, num_gen=5000)
+    return dict(fid5k=fid)
+
+@register_metric
+def fid10k(opts):
+    opts.dataset_kwargs.update(max_size=None, xflip=False)
+    fid = frechet_inception_distance.compute_fid(opts, max_real=None, num_gen=10000)
+    return dict(fid10k=fid)
+
+@register_metric
 def fid50k_full(opts):
     opts.dataset_kwargs.update(max_size=None, xflip=False)
     fid = frechet_inception_distance.compute_fid(opts, max_real=None, num_gen=50000)
     return dict(fid50k_full=fid)
+
+@register_metric
+def kid1k(opts):
+    opts.dataset_kwargs.update(max_size=None, xflip=False)
+    kid = kernel_inception_distance.compute_kid(opts, max_real=1000, num_gen=1000, num_subsets=100, max_subset_size=1000)
+    return dict(kid1k=kid)
+
+@register_metric
+def kid5k(opts):
+    opts.dataset_kwargs.update(max_size=None, xflip=False)
+    kid = kernel_inception_distance.compute_kid(opts, max_real=5000, num_gen=5000, num_subsets=100, max_subset_size=1000)
+    return dict(kid5k=kid)
+
 
 @register_metric
 def kid50k_full(opts):
